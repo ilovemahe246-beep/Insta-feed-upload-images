874b459c2c66809b008fcb0f381440774cd8eb0e
Maheen Shakeel <ilovemahe246@gmail.com>
2026-07-05 13:56:55 +0500

    Add default route to React app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import CreatePost from './pages/CreatePost';
-import Feed from "./pages/feedpage"; 
-export const App = () => {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import CreatePost from "./pages/CreatePost";
+import Feed from "./pages/feedpage";
+
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/create-post' element ={<CreatePost />}/>
-        <Route path='/feed' element ={<Feed />} />  
+        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </Router>
-  )
+  );
 }
-export default App
